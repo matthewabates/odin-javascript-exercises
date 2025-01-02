ff3ea2c879e45a570e4b64f3b271c762c940165d
matthewabates <matthew.a.bates@gmail.com>
2025-01-01 18:04:04 -0600

    first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) return "ERROR"
+    
+    let result = "";
+    [...Array(times)].forEach(e => {
+        result += str
+    })
+    return result
 };
 
 // Do not edit below this line
