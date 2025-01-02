2f2f50ce7246e902baaf413ef40c3c8da9175e25
matthewabates <matthew.a.bates@gmail.com>
2025-01-01 23:18:39 -0600

    exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return round((f - 32) * 5/9)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return round((c * 9/5) + 32)
 };
+
+function round(n) {
+  return Math.round(n*10)/10
+}
 
 // Do not edit below this line
 module.exports = {
