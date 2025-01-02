b51dd4fd17a0de190bb3024d9d51500fac74a08a
matthewabates <matthew.a.bates@gmail.com>
2025-01-02 16:03:04 -0600

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people
+        .map(person => {            
+            person.age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth
+            return person
+        })
+        .sort((a,b) => {
+            return b.age - a.age
+        })[0]
 };
 
 // Do not edit below this line
