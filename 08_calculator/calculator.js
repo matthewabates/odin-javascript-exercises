6bcd80878f8d994c76aa3bc6c9303613ee8e5108
matthewabates <matthew.a.bates@gmail.com>
2025-01-02 14:40:46 -0600

    exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((acc,n) => acc+n, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((acc, n) => acc*n)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let nums = Array.from(Array(x).keys()).map(n=>n+1)
+  return nums.reduce((acc, n) => acc*n, 1)
 };
 
 // Do not edit below this line
