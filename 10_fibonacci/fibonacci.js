8eb86fa6345c82d1be5cdf9bdfa04336001931ee
matthewabates <matthew.a.bates@gmail.com>
2025-01-02 15:49:40 -0600

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if (n<0) return "OOPS"
+    n = parseInt(n)
+    let fib = [...Array(n+1)].reduce((acc, _, i) => {
+        if (i < 2) {
+            return acc.concat(i);
+        } else {
+            return acc.concat(acc[i-1] + acc[i-2])
+        }
+    }, []) 
+    return fib[n]
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
