d4b4744d29bc99a2cf38f18d9fb1271d2d6045c4
matthewabates <matthew.a.bates@gmail.com>
2025-01-02 14:58:36 -0600

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase()    
+    letters = word.split("").filter(isAlphaNumerical)
+    return letters.join('') === letters.reverse().join('')
 };
+
+function isAlphaNumerical(c) {
+    return 'abcdefghijklmnopqrstuvwxyz0123456789'.includes(c) 
+}
 
 // Do not edit below this line
 module.exports = palindromes;
