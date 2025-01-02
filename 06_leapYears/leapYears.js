0d537006b85f5f8fa32c8fbcd30eb939d053151c
matthewabates <matthew.a.bates@gmail.com>
2025-01-01 23:05:43 -0600

    exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isYearDivisibleByFour = !(year % 4)
+    let isCentury = !(year % 100)
+    let isFourHundred = !(year % 400)
+    return (isYearDivisibleByFour && (!isCentury || isFourHundred))
 };
 
 // Do not edit below this line
